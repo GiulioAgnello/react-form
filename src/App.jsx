9141879e19c59a51a691a3c026b3b199d1a879c5
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-26 15:43:19 +0200

    onchange

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
+// funzioni
+const newsTitle = "titolo primo";
+const handelchange = (e) => {
+  e.preventDefault();
+  console.log(e);
+};
 
 export default function App() {
   return (
     <>
       <h1 className="d-flex justify-content-center p-4">FORM</h1>
       <form>
-        <input type="text" value="valore" />
+        <input onChange={handelchange} type="text" value={newsTitle} />
         <div className="card mt-3">
           <ul>
             <li>testo</li>
