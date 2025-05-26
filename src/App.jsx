4be8b75cafb1ea49cc7e8d02eeb07bf9d19dc02c
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-26 17:10:58 +0200

    end_baseMilstone

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
 import { useState } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Main from "./components/Main";
 // funzioni
-const [newsTitle, setNewtitle] = useState("");
-
-function handelchange(e) {
-  e.preventDefault();
-  setNewtitle(e.target.value);
-  // console.log(e.target.value);
-}
 
 export default function App() {
+  const [titleList, setTitleList] = useState([]);
+  const [newsTitle, setNewtitle] = useState("");
+
+  function handelchange(e) {
+    setNewtitle(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newListTitle = [...titleList, newsTitle];
+    console.log(newListTitle);
+    setTitleList(newListTitle);
+  }
+
   return (
     <>
       <h1 className="d-flex justify-content-center p-4">FORM</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input onChange={handelchange} type="text" value={newsTitle} />
         <div className="card mt-3">
           <ul>
-            <li>testo</li>
+            {titleList.map((title, i) => {
+              return <li key={i}>{title}</li>;
+            })}
           </ul>
         </div>
         <div className="d-flex justify-content-center mt-3">
